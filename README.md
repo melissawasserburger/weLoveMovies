ab71316e4862611b98c6b0fa3464f2d70d1744db
Melissa Wasserburger <mwasserburger@outlook.com>
2022-08-22 12:33:24 -0400

    added README

---
+++ README.md
@@ -0,0 +1,11 @@
+# WeLoveMovies Backend Application
+This is the backend counterpart for the WeLoveMovies application.
+
+This project was a great exercise in express servers, database setup and communications and cors.
+
+Included routes for the project are:
+/movies
+/movies/:movieId
+/reviews
+/reviews/:reviewId
+/theaters
--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const reviewsRouter = require("./reviews/reviews.router");
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
